22733077e84bf6fdf96eaeee2e4fbe86a18c5492
Gaelle Bono <gaelle.bono@free.fr>
2026-03-23 17:06:20 +0100

    adding login-page with forgot password feature

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -43,7 +43,7 @@ class RegistrationController extends AbstractController
             $mailService->sendMail($user, 'Bienvenue sur Vite et Gourmand!', 'emails/welcome.html.twig', ['user' => $user]);
 
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_login');
         }
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,7 +64,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setEmail(string $email): static
     {
-        $this->email = $email;
+        $this->email = strtolower($email);
 
         return $this;
     }
